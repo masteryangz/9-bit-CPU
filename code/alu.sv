156913a440c0c5f3b4a2984cf1859c6528fbbe40
masteryangz <yangzuo2001@gmail.com>
2024-12-21 00:27:06 -0800

    modified for carry in out

--- code/alu.sv
+++ code/alu.sv
@@ -3,17 +3,18 @@
 module alu(
   input[2:0] ALUOp,    // ALU instructions
   input[7:0] inA, inB,	 // 8-bit wide data path
-  //input      sc_i,       // shift_carry in
+  input      carry_in,       //add carry
   output logic[7:0] rslt,
   //output logic sc_o,     // shift_carry out
   // reduction XOR (output)
+  output logic carry_out,
 	output logic taken     // NOR (output)
 );
 //logic[inB-1:0] shift;
 always_comb begin 
-  rslt = 'b0;            
+  //rslt = 'b0;            
   //sc_o = 'b0;    
-  taken = 'b0;
+  //taken = 'b0;
   case(ALUOp)
 /*
     3'b001: begin//lsf
@@ -21,25 +22,57 @@ always_comb begin
 		  rslt = {shift, inA[7:inB]};
 	  end
 */
-    3'b000: //ld
+    3'b000: begin//ld
       rslt = inB;
-    3'b001: rslt = inA >>> 1;
-    3'b010: rslt = inA + inB;
-	  3'b011: //pos
-	    if(inA[7]==1) taken = 1;
+      carry_out = 1'b0;
+      taken = 1'b0;
+    end
+    3'b001: begin
+      rslt = inA >>> 1;
+      carry_out = 1'b0;
+      taken = 1'b0;
+    end
+    3'b010: begin
+      {carry_out, rslt} = inA + inB + carry_in;
+      taken = 1'b0;
+    end
+	  3'b011: begin//pos
+      rslt = 8'b0000_0000;
+	    if(inA[7]==1) taken = 1'b1;
+      else taken = 1'b0;
+      carry_out = 1'b0;
+    end
       /*begin
 		rslt[7:1] = ina[6:0];
 		rslt[0]   = sc_i;
 		sc_o      = ina[7];
       end*/
-    3'b100: // bitwise XOR
+    3'b100: begin// bitwise XOR
 	    rslt = inA ^ inB;
-	  3'b101: //beq
-	    if(inA==0) taken = 1;
-    3'b110: //lw
+      taken = 1'b0;
+      carry_out = 1'b0;
+    end
+	  3'b101: begin//beq
+      rslt = 8'b0000_0000;
+	    if(inA==0) taken = 1'b1;
+      else taken = 1'b0;
+      carry_out = 1'b0;
+    end
+    3'b110: begin//lw
       rslt = inB;
-    3'b111: //sw
+      taken = 1'b0;
+      carry_out = 1'b0;
+    end
+    3'b111: begin//sw
       rslt = inB;
+      taken = 1'b0;
+      carry_out = 1'b0;
+    end
+    default: begin
+      rslt = 8'b0000_0000;
+      taken = 1'b0;
+      carry_out = 1'b0;
+    end
   endcase
 end
    
