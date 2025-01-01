de32b500acd50eaf4390444f42bf7e5d05441ad3
masteryangz <yangzuo2001@gmail.com>
2025-01-01 14:07:38 -0800

    arithmetic finished

--- code/PC_LUT.sv
+++ code/PC_LUT.sv
@@ -4,13 +4,13 @@ module PC_LUT #(parameter D=12)(
 
   always_comb case(how_high)
     0: target = 2;  
-    1: target = 15;//6;
+    1: target = 15;//
 	2: target = 50;//k loop
-	3: target = -41;//-26back to k loop
-	4: target = 371;//184j loop
-	5: target = -375;//-173back to j loop
-	6: target = 401;//216i loop
-	7: target = -411;//-209back to i loop
+	3: target = -41;//back to k loop
+	4: target = 371;//j loop
+	5: target = -375;//back to j loop
+	6: target = 415;//i loop
+	7: target = -425;//back to i loop
 	default: target = 'b0;  // hold PC  
   endcase
 
